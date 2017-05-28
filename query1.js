978c5dc3fe2ca3e0f35fd531e7fa828ba7d412ef
NataljaD <nata737mail@gmail.com>
2017-05-28 12:24:44 +0300

    Fixed query1

--- query1.js
+++ query1.js
@@ -1,6 +1,10 @@
-db.people.find({
-    'scores.score' :{
-        $gt:87,
-        $lt:93
-    }
+db.students.find({
+	scores: { 
+		"$elemMatch": { 
+			score: { 
+				$gt: 87, 
+				$lt: 93 
+			} 
+		} 
+	} 
 }).pretty()
